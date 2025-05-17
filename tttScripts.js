7b59a63e7fd6be2ea94e22380ca289c529078e30
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-05-17 07:59:53 -0500

    Create reset board function in gameboard

--- tttScripts.js
+++ tttScripts.js
@@ -1,5 +1,5 @@
 const Gameboard = (function () {
-    const board = ["", "", "", "", "", "", "", "", ""];
+    const board = ["x", "x", "x", "x", "o", "o", "o", "o", "o"]; // Filled with dummy markers
 
     const getBoard = () => board; // Gets board - leaves actual board array private
 
@@ -7,8 +7,15 @@ const Gameboard = (function () {
         board[index] = marker;
     };
 
+    const resetBoard = () => { // Loop through board, reset cells
+        for (let i = 0; i < board.length; i++) {
+            board[i] = "";
+            console.log("clear");
+        }
+    };
+
     return { 
-        getBoard, placeMarker
+        getBoard, placeMarker, resetBoard
     }
 
 })();
