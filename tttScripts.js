f8759fedc871ddf13af95c2144865204f60790d1
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-05-17 07:56:22 -0500

    Create placeMarker function in gameboard

--- tttScripts.js
+++ tttScripts.js
@@ -3,7 +3,13 @@ const Gameboard = (function () {
 
     const getBoard = () => board; // Gets board - leaves actual board array private
 
-    return { getBoard }
+    const placeMarker = (index, marker) => { // Places player marker at index position
+        board[index] = marker;
+    };
+
+    return { 
+        getBoard, placeMarker
+    }
 
 })();
 
