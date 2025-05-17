b0d36572d5abb8e5c01be4c64ab2e28bc6f1203f
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-05-17 07:55:28 -0500

    Create gameboard module

--- tttScripts.js
+++ tttScripts.js
@@ -1,1 +1,10 @@
+const Gameboard = (function () {
+    const board = ["", "", "", "", "", "", "", "", ""];
 
+    const getBoard = () => board; // Gets board - leaves actual board array private
+
+    return { getBoard }
+
+})();
+
+console.log(Gameboard.getBoard);
